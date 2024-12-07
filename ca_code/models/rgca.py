e64e8b0475e22523cce3d8d892a80f4559713192
Jonathan Schmidt <jonathan.schmidt@tum.de>
2024-12-07 21:44:06 +0100

    Scale decoder output

--- ca_code/models/rgca.py
+++ ca_code/models/rgca.py
@@ -334,7 +334,7 @@ class GeomDecoder(nn.Module):
     def forward(self, embs: th.Tensor) -> Dict[str, th.Tensor]:
         preds = {}
 
-        geom = self.geommod(embs).view(embs.shape[0], -1, 3)
+        geom = self.geommod(embs).view(embs.shape[0], -1, 3) * 1e-3 # NOTE: We scale this since our geometry is in meters (not mm)
         geom = geom * self.verts_std + self.verts_mean
 
         preds.update(face_geom=geom)
@@ -455,7 +455,7 @@ class PrimDecoder(nn.Module):
         # Gaussian parameters
         f_geom = f_vnocond[:, self.n_diff_coeffs : self.n_diff_coeffs + 11]
         f_geom = f_geom.permute(0, 2, 3, 1).view(B, -1, 11)
-        primpos = f_geom[..., 0:3] + primposbase
+        primpos = f_geom[..., 0:3] * 0.001 + primposbase # NOTE: We scale this since our geometry is in meters (not mm)
         primqvec = F.normalize(f_geom[..., 3:7], dim=-1)
         primscale = F.softplus(f_geom[..., 7:10])
         opacity = th.sigmoid(f_geom[..., 10:11])
@@ -476,7 +476,6 @@ class PrimDecoder(nn.Module):
         albedo = self.albedo.expand(B, -1, -1)
 
         # compute diffuse color
-        print(albedo.shape, diff_shs.shape, headrel_light_sh.shape)
         diff_color = albedo * (diff_shs * headrel_light_sh[:, None]).sum(dim=-1)
 
         # compute specular color
